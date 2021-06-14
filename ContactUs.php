16354c2d1d7fb65746e4ca021186bf265bb3ac8b
Sumana M R <73119135+sumanamr@users.noreply.github.com>
2021-06-14 22:56:42 +0530

    Contact Us page

---
+++ ContactUs.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href = "style1.css">
+    </head>
+    <body>
+      <?php 
+         include 'header.php'; ?> 
+         <br>
+         <br>
+
+         <div class = "contactus">
+        <div class = "contact">
+           For any Suggestions/Comments/Feedback please email us 
+           <br>
+           info@sparksbank.com
+        </div>
+        <div class = "contacts">   
+            Customer Care Number:<br>
+            1800 852 962(Toll free)
+            <br>
+            1800 752 500(Toll free)
+            <br>
+            080-2635 8100
+        </div>
+        </div>
+        <img class= "img" src= "images/contactus.jpg" height = "300"/>
+
+    </body>
+</html>
