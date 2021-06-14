9ce161ecb61dbb44abf1780188253296ccadd4d9
Sumana M R <73119135+sumanamr@users.noreply.github.com>
2021-06-14 22:57:27 +0530

    navbar

---
+++ header.php
@@ -0,0 +1,25 @@
+<div class="main">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <img src="images/bank.jpg"  style = "border : 'white' ; border-radius:50%;" width=50 height=50;/><a class="navbar-brand" href="#">&nbsp; SPARKS BANK</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarmenu"></button>
+         
+    <div class="collapse navbar-collapse" id="navbarmenu"> 
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="Home.php">Home&emsp;&emsp;</a>
+             </li>
+            <li class="nav-item">
+               <a href="Customers.php" class="nav-link">View Customers&emsp;&emsp;</a>
+           </li>
+           <li class="nav-item">
+            <a href="TransactionHistory.php" class="nav-link">Transaction History&emsp;&emsp;</a>
+         </li>
+           <li class="nav-item">
+              <a href="About.php" class="nav-link">About Us&emsp;&emsp;</a>
+           </li>
+
+        </ul>  
+    </div>
+    </nav>
+</div>
+
