bf7bf9929e832d8b9f46a8229acfd52c53d9da6a
Sumana M R <73119135+sumanamr@users.noreply.github.com>
2021-06-14 22:56:19 +0530

    About Us page

---
+++ About.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<div class="aboutus">
+<body>
+   <?php 
+      include 'header.php';
+    ?>  
+        <br>
+        <br>
+        <div class="imgabout">
+        <img id = "imgab" src = "images/hand-giving-money-other-internet-260nw-253279837.jpg" height = "300" width= "300"/>
+        &nbsp;&nbsp;
+       
+        <div class ="about">
+        <p>
+            Sparks Bank (India) offers a diverse range of banking solutions in the Indian market, and aims to make banking easier and life more comfortable. Transaction between customers made in one click.Sparks bank gives you th option of transferring money from one customer to the other.
+        </p>
+       </div>
+       &nbsp;
+       <img id = "transfer" src="images/transfer.jpg" height="300" width="300"/>
+      </div>
+      <p id = "h">
+          Sparks Bank started in the year 2020 and has been successful since then. It's user-friendly options and easy way to transfer money has made it popular.
+      </p>
+      <img id = "history" src = "images/history.jpg" height="300" width="300"/>
+
+
+</body>
+</div>
+</html>
